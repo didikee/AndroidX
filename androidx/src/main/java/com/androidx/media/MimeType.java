98723961b3712879e1c77e8f91273493e0047cb7
didikeeLuanon <wwh@lunaon.com>
2020-06-19 15:45:46 +0800

    增加webp的mimetype

--- androidx/src/main/java/com/androidx/media/MimeType.java
+++ androidx/src/main/java/com/androidx/media/MimeType.java
@@ -20,6 +20,7 @@ public final class MimeType {
     public static final String PNG = "image/png";
     public static final String JPEG = "image/jpeg";
     public static final String GIF = "image/gif";
+    public static final String WEBP = "image/webp";
     // video
     public static final String VIDEO = "video/*";
     public static final String MP4 = "video/mp4";
