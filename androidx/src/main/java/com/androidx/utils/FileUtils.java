6375ec6c0e547678fcce421ce66b646e4ded5740
didikeeLuanon <wwh@lunaon.com>
2020-09-15 15:43:44 +0800

    相册更新

--- androidx/src/main/java/com/androidx/utils/FileUtils.java
+++ androidx/src/main/java/com/androidx/utils/FileUtils.java
@@ -1,9 +1,42 @@
 package com.androidx.utils;
 
+import android.content.Context;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+
+import com.androidx.LogUtils;
+import com.androidx.media.MimeType;
+
+import java.io.File;
+
 /**
  * user author: didikee
  * create time: 9/15/20 2:01 PM
  * description: 
  */
-class FileUtils {
+public final class FileUtils {
+    private static final MediaScannerConnection.OnScanCompletedListener ON_SCAN_COMPLETED_LISTENER = new MediaScannerConnection.OnScanCompletedListener() {
+        @Override
+        public void onScanCompleted(String path, Uri uri) {
+            LogUtils.d("FileUtils --> scanFile path: " + path + " uri: " + (uri == null ? " " : uri.toString()));
+        }
+    };
+
+    public static void scanFile(Context context, File file) {
+        if (file != null) {
+            String mimeTypeFromFilename = MimeType.getMimeTypeFromFilename(file.getName());
+            scanFile(context, file, mimeTypeFromFilename, null);
+        }
+    }
+
+    public static void scanFile(Context context, File file, String mimeType, MediaScannerConnection.OnScanCompletedListener onScanCompletedListener) {
+        if (context == null) {
+            LogUtils.d("FileUtils --> scanFile context is null.");
+            return;
+        }
+        if (file == null || !file.exists()) {
+            return;
+        }
+        MediaScannerConnection.scanFile(context, new String[]{file.getAbsolutePath()}, new String[]{mimeType}, onScanCompletedListener == null ? ON_SCAN_COMPLETED_LISTENER : onScanCompletedListener);
+    }
 }
