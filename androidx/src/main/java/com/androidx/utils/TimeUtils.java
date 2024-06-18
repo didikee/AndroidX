7f1f7e9d355ea8b4e222e8c94e80ecb52f791060
didikee <didikee@qq.com>
2024-06-18 10:11:51 +0800

    Update TimeUtils.java

--- androidx/src/main/java/com/androidx/utils/TimeUtils.java
+++ androidx/src/main/java/com/androidx/utils/TimeUtils.java
@@ -2,6 +2,9 @@ package com.androidx.utils;
 
 import android.text.format.DateFormat;
 
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * description: 关于时间的工具类
  */
@@ -19,5 +22,43 @@ public final class TimeUtils {
 
     public static CharSequence format(CharSequence format, long timeInMillis) {
         return DateFormat.format(format, timeInMillis);
+    }
+
+    public static String formatTimestamp(long timeInMillis) {
+        if (timeInMillis <= 0) {
+            return "";
+        }
+        java.text.DateFormat instance = java.text.DateFormat.getDateTimeInstance(java.text.DateFormat.LONG,
+                java.text.DateFormat.MEDIUM,
+                Locale.getDefault());
+        return instance.format(new Date(timeInMillis));
+    }
+
+
+    /**
+     * icu下的android.icu.text.DateFormat 和 java.text.DateFormat
+     *
+     * @param timeInMillis
+     * @return
+     */
+    public static String formatDateInMultiLanguage(Long timeInMillis) {
+        if (timeInMillis <= 0) {
+            return "";
+        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            android.icu.text.DateFormat df = android.icu.text.DateFormat.getDateTimeInstance(android.icu.text.DateFormat.LONG,
+//                    android.icu.text.DateFormat.MEDIUM, Locale.getDefault());
+//            return df.format(new Date(timeMs));
+//        } else {
+//            java.text.DateFormat instance = java.text.DateFormat.getDateTimeInstance(java.text.DateFormat.LONG,
+//                    java.text.DateFormat.LONG,
+//                    Locale.getDefault());
+//            return instance.format(new Date(timeMs));
+//        }
+
+        java.text.DateFormat instance = java.text.DateFormat.getDateTimeInstance(java.text.DateFormat.LONG,
+                java.text.DateFormat.MEDIUM,
+                Locale.getDefault());
+        return instance.format(new Date(timeInMillis));
     }
 }
